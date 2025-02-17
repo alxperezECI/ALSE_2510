7a15262320c6234c9bf325e7a92ee7895171e1ab
juan david nova <juan.nova-g@mail.escuelaing.edu.co>
2025-02-17 11:28:32 -0500

    tarea Jostin C. juan N.

--- eje4/vectores.cpp
+++ eje4/vectores.cpp
@@ -9,30 +9,47 @@ using namespace std;
 double punto2( const double x[], const double y[], double r[], const int &cant ){
   // En esta función se hace el producto elemento a elemento de los dos vectores y se devuelve
   // la suma de todos los valores del vector de resultados.
-
-  return 0;
+  double suma = 0;
+  for ( int i=0; i< cant; i++){
+    r[i] = x[i] * y[i];
+    suma += r[i];
+  }
+  return suma;
 }
 
 double punto1( double *x, double *y, double *r, const int &cant ){
   // En esta función se hace el producto elemento a elemento de los dos vectores y se devuelve
   // la suma de todos los valores del vector de resultados.
-
-  return 0;
+  double suma = 0;
+  for ( int i=0; i< cant; i++){
+      *(r+i) = *(x+i) * *(y+i);
+      suma += *(r+i);
+  }
+  return suma;
 }
 
+void producto_punto(){
+  srand(time(0));
+  double v1[100], v2[100], vres[100];
+
+  for (int i = 0; i < 100; i++) {
+       v1[i] = 3.0;
+  }
+
+  for(int i=0; i<100;i++){
+    v2[i] = 50 + rand() % 100;
+  }
+
+  cout << "La suma total con punto1 es: " <<  punto1(v1, v2, vres, 100) << endl ;
+  cout << "La suma total con punto2 es: " <<  punto2(v1, v2, vres, 100) << endl ;
+
+  for(int i=0; i<100; i++){
+    cout << "vres[" << i << "] = " << vres[i] << endl;
+  }
+}
 
 int main(){
-  srand(time(0));
-  double v1[100]={3.}, v2[100], vres[100];
-
-  for(int i=0; i<100;i++)
-    v2[i] = 50 + rand() % 100;
-
-  cout << "La suma total con punto1 es: " <<  punto1(v1, v2, vres, 100);
-  cout << "La suma total con punto2 es: " <<  punto2(v1, v2, vres, 100);
-
-  for(int i=0; i<100;i++)
-    cout << "vres[" << i << "] = " << vres[i] << endl;
-
+  producto_punto();
   return 0;
 }
+
