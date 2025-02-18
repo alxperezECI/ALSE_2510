30d4a7b8a4a27a987a419c8434f979530c3d9011
sergio plazas <sergio.plazas-c@mail.escuelaing.edu.co>
2025-02-18 13:07:55 +0000

    Tarea ALSE punteros y vectores

--- eje4/vectores.cpp
+++ eje4/vectores.cpp
@@ -9,25 +9,39 @@ using namespace std;
 double punto2( const double x[], const double y[], double r[], const int &cant ){
   // En esta función se hace el producto elemento a elemento de los dos vectores y se devuelve
   // la suma de todos los valores del vector de resultados.
-
-  return 0;
-}
+  double suma = 0;
+  for (int i = 0; i <= cant; i++) {
+        r[i] = x[i] * y[i];
+        suma += r[i];
+  };
+  return suma;
+  
+};
 
 double punto1( double *x, double *y, double *r, const int &cant ){
   // En esta función se hace el producto elemento a elemento de los dos vectores y se devuelve
   // la suma de todos los valores del vector de resultados.
-
-  return 0;
-}
+  double a=0, b=0;
+  for(int i=0; i <= cant;i++){
+      a= *(x+i);
+      b= *(y+i);
+      *(r+i) = a * b;
+  };
+  double suma=0;
+ for(int i=0; i <= cant;i++){
+    suma=suma + *(r+i); 
+ };
+ return suma;
+};
 
 
 int main(){
   srand(time(0));
   double v1[100]={3.}, v2[100], vres[100];
 
-  for(int i=0; i<100;i++)
+  for(int i=0; i<100;i++){
     v2[i] = 50 + rand() % 100;
-
+  };
   cout << "La suma total con punto1 es: " <<  punto1(v1, v2, vres, 100);
   cout << "La suma total con punto2 es: " <<  punto2(v1, v2, vres, 100);
 
