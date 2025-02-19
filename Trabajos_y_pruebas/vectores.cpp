b807c8876c5b257cd020a98ac0ea4294722137ad
sergio plazas <sergio.plazas-c@mail.escuelaing.edu.co>
2025-02-19 18:43:34 +0000

    version nueva

--- Trabajos_y_pruebas/vectores.cpp
+++ Trabajos_y_pruebas/vectores.cpp
@@ -21,16 +21,14 @@ double punto2( const double x[], const double y[], double r[], const int &cant )
 double punto1( double *x, double *y, double *r, const int &cant ){
   // En esta función se hace el producto elemento a elemento de los dos vectores y se devuelve
   // la suma de todos los valores del vector de resultados.
-  double a=0, b=0;
-  for(int i=0; i <= cant;i++){
-      a= *(x+i);
-      b= *(y+i);
-      *(r+i) = a * b;
+  for(int i=0; i < cant;i++){
+      *r = *x * *y;
   };
   double suma=0;
- for(int i=0; i <= cant;i++){
-    suma=suma + *(r+i); 
+ for(int i=0; i < cant;i++){
+    suma=suma + *r; 
  };
+ x++;y++,r++;
  return suma;
 };
 
@@ -40,6 +38,7 @@ int main(){
   double v1[100]={3.}, v2[100], vres[100];
 
   for(int i=0; i<100;i++){
+    v1[i]=3.0;
     v2[i] = 50 + rand() % 100;
   };
   cout << "La suma total con punto1 es: " <<  punto1(v1, v2, vres, 100);
