d68b2d57a15d6ea40f662b6d79d16238957f17cf
JuanCor253 <juan.cortes-gu@mail.escuelaing.edu.co>
2025-02-19 13:22:45 -0500

    Cambio funcion paso por puntero

--- eje4/vectores.cpp
+++ eje4/vectores.cpp
@@ -24,8 +24,8 @@ double punto1( double *x, double *y, double *r, const int &cant ){
   double aux = 0.;
 
   for(int i = 0; i < cant; i++){
-    r[i] = x[i] * y[i];
-    aux += r[i];
+    *r = *x * *y;
+    aux += *r;
   }
     return aux;
 }
@@ -35,11 +35,10 @@ int main(){
   double v1[100], v2[100], vres[100];
 
 //Este ciclo se usa para inicializar todos los valores del v1 con el valor de 3.0
-  for(int i=0; i<100;i++)
+  for(int i=0; i<100;i++){
     v1[i] = 3.0;
-
-  for(int i=0; i<100;i++)
     v2[i] = 50 + rand() % 100;
+  }
 
   cout << "La suma total con punto1 es: " <<  punto1(v1, v2, vres, 100) << endl;
   cout << "La suma total con punto2 es: " <<  punto2(v1, v2, vres, 100) << endl;
