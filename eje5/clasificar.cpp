b3405aa01a6e0605c70cbe68c9bbb7408360eb44
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-03-06 14:23:55 -0500

    Agregando archivo de ejercicio 5.

--- eje5/clasificar.cpp
+++ eje5/clasificar.cpp
@@ -3,12 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main(int argc, char** argv){
   cout << "He recibido " << argc << " argumentos de los cuales: " << endl;
   for(int i = 0; i < argc; i++){
+    for( int j=0; j<strlen(argv[i]); j++){
+      if( isdigit(argv[i][j]) ){
+        cout << "El argumento " << i << " es un numero" << endl;
+        break;
+      }
+    }
     cout << argv[i] << endl;
   }
   return 0;
