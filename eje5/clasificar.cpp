b0829beb2d39304328736c746caad5f79f9ef715
Andres Rubio <andres.rubio-c@mail.escuelaing.edu.co>
2025-03-18 13:34:06 -0500

    Tareas Clasificar y Polinomios

--- eje5/clasificar.cpp
+++ eje5/clasificar.cpp
@@ -1,23 +1,32 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
 #include <iostream>
-#include <string.h>
-
+#include <string>
 using namespace std;
 
-int main(int argc, char** argv){
-  cout << "He recibido " << argc << " argumentos de los cuales: " << endl;
-  for(int i = 0; i < argc; i++){
-    for( int j=0; j<strlen(argv[i]); j++){
-      if( isdigit(argv[i][j]) ){
-        cout << "El argumento " << i << " es un numero" << endl;
-        break;
-      }
+// GRUPO: SAMUEL MORENO Y ANDRES RUBIO
+
+bool letras(const string &palabra)
+{
+  for (char c : palabra)
+  {
+    if (!isalpha(c))
+      return false;
+  }
+  return true;
+}
+
+int main(int argc, char *argv[])
+{
+  cout << "Hay " << argc - 1 << " argumentos de los cuales: " << endl;
+
+  for (int i = 1; i < argc; i++)
+  {
+    string argumento = argv[i];
+
+    if (letras(argumento))
+    {
+      cout << "La longitud es " << argumento.length()
+           << " y el argumento es " << argumento << endl;
     }
-    cout << argv[i] << endl;
   }
   return 0;
 }
-
