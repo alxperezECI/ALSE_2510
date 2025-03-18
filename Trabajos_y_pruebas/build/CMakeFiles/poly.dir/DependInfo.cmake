f44e025d1b6c202a5d19cbac9d0240b0ceb65c04
sergio plazas <sergio.plazas-c@mail.escuelaing.edu.co>
2025-03-18 03:04:44 +0000

    actualizacion_temporal

---
+++ Trabajos_y_pruebas/build/CMakeFiles/poly.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/main_user/Proyectos/ALSE_2510/Trabajos_y_pruebas/poly.cpp" "CMakeFiles/poly.dir/poly.cpp.o" "gcc" "CMakeFiles/poly.dir/poly.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
