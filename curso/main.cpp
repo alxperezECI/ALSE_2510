d2f91f0cfd34da81774534188a0468ce9e998f55
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-03-19 21:19:10 -0500

    Clases Persona y Estudiante terminadas. Falta terminar el main.

--- curso/main.cpp
+++ curso/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include "estudiante.h"
 #include "persona.h"
+#include <vector>
+#include <fstream>
+#include <string>   
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
+    if(argc != 2)
+    {
+        cout << "Uso: " << argv[0] << " <archivo de datos>" << endl;
+        return 1;
+    }
     vector<Estudiante> estudiantes;
     double promedio_curso = 0.0;
 
-    Estudiante e1("Juan", "Perez", 123456, 1);
-    e1.agregarNota(3.5);
-    e1.agregarNota(4.5);
-    e1.agregarNota(5.0);
-    cout << e1 << endl;
+    // Se leeerá de un archivo en donde en cada línea se tiene el nombre, apellido, documento de identidad y código del estudiante
+    // Se leerán las notas de cada estudiante y se calculará el promedio del curso  
+    ifstream archivo(argv[1]);
+    string nombre, apellido;
+    int docid, codigo;
+    double nota[5];
+    while(archivo >> codigo >> nombre >> apellido >> docid  >> nota[0] >> nota[1] >> nota[2] >> nota[3] >> nota[4])
+    {
+      // Poner aquí el código para crear un objeto de la clase Estudiante y agregarlo al vector estudiantes
+    }
+    archivo.close();
 
-    estudiantes.push_back(e1);
+    cout << "No. de estudiantes cargados: " << estudiantes.size() << endl;
     
     vector<Estudiante>::iterator it;
     for(it = estudiantes.begin(); it != estudiantes.end(); it++)
@@ -23,6 +37,5 @@ int main()
     }
     cout << "Promedio del curso: " << promedio_curso / estudiantes.size() << endl;  
 
-    
     return 0;
 }
